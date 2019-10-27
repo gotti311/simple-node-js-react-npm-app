231d1bedbf89c7a504deae76161955fbed3e0eb2
AG <agott311@hotmail.com>
2019-10-27 14:13:05 -0500

    getting node/npm version in container

--- jenkins/scripts/test.sh
+++ jenkins/scripts/test.sh
@@ -13,6 +13,11 @@ set -x
 npm install --save-dev cross-env
 set +x
 
+echo 'node version....'
+node -v
+echo 'npm version...'
+npm -v
+
 echo 'The following "npm" command tests that your simple Node.js/React'
 echo 'application renders satisfactorily. This command actually invokes the test'
 echo 'runner Jest (https://facebook.github.io/jest/).'
